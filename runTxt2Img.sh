c600c188079a912e68b78ee3c1eef76bb4a525cb
mw <694497013@qq.com>
2025-03-07 04:23:52 +0000

    [opt]添加依赖

---
+++ runTxt2Img.sh
@@ -0,0 +1,1 @@
+python scripts/txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt /content/Python_sd_web/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  
