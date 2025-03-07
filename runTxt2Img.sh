6ec344aa81f5ceee0457952f63bfa854f2ae9d5f
mw <694497013@qq.com>
2025-03-07 08:55:35 +0000

    [opt] 适配cuda

--- runTxt2Img.sh
+++ runTxt2Img.sh
@@ -1,2 +1,4 @@
 source /content/Python_sd_web/venv/bin/activate
 python txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt /content/Python_sd_web/repositories/stable-diffusion-stability-ai/trinart2_step115000.ckpt --config configs/stable-diffusion/v2-inference.yaml --H 768 --W 768  
+
+# python txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --device cuda --ckpt /content/Python_sd_web/repositories/stable-diffusion-stability-ai/trinart2_step115000.ckpt --config configs/stable-diffusion/v2-inference.yaml --H 768 --W 768  
