18e19f6adaab0c19898169ede95f163639d08e30
mw <694497013@qq.com>
2025-03-07 07:21:14 +0000

    [opt]修改模型路径

--- runTxt2Img.sh
+++ runTxt2Img.sh
@@ -1,1 +1,1 @@
-python txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt /content/Python_sd_web/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors --config configs/stable-diffusion/v2-inference.yaml --H 768 --W 768  
+python txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt /content/Python_sd_web/models/Stable-diffusion/v1-5-pruned-emaonly.ckpt --config configs/stable-diffusion/v2-inference.yaml --H 768 --W 768  
